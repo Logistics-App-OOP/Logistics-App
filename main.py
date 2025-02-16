0f4954226a4a61b9c778abb02dc2421127b4c400
uasim1702 <uasim1702@abv.bg>
2025-02-16 21:02:27 +0200

    more commands in main

--- main.py
+++ main.py
@@ -11,7 +11,47 @@ engine.start()
 
 
 # registeremployee Uasim1702 Uasim Halak 123456789 Manager
+# registeremployee JohnDoe John Doe 987654321 Normal
+# registeremployee AliceSmith Alice Smith 456123789 Manager
+# login JohnDoe 987654321
+# login AliceSmith 456123789
+
 # login Uasim1702 123456789
+
+# createpackage PeterParker 0411223344 Melbourne Brisbane 30
+# createpackage MaryJane 0400112233 Perth Darwin 55
+# createpackage TonyStark 0433221100 Adelaide Sydney 70
+# createpackage SteveRogers 0455667788 Sydney Perth 90
+# createpackage NatashaRomanoff 0499887766 Brisbane AliceSprings 40
+
+# createroute 2025-02-20T09:00 Sydney Melbourne Adelaide
+# createroute 2025-02-21T12:45 Brisbane Perth
+# createroute 2025-02-22T06:30 Darwin AliceSprings Sydney
+# createroute 2025-02-23T15:10 Adelaide Melbourne Sydney Brisbane
+# createroute 2025-02-24T08:00 Perth Melbourne Sydney Brisbane AliceSprings Darwin
+
+# assigntrucktoroute 3
+# assigntrucktoroute 4
+# assigntrucktoroute 5
+
+
+# assignpackagetoroute 3 3
+# assignpackagetoroute 4 4
+# assignpackagetoroute 5 5
+# assignpackagetoroute 2 3
+# assignpackagetoroute 1 4
+
+# findroutes Melbourne Brisbane
+# findroutes Darwin Sydney
+# findroutes Adelaide Sydney
+# findroutes Perth AliceSprings
+
+# findpackage 1
+# findpackage 2
+# findpackage 5
+# findpackage 7
+
+
 # createpackage IvanIvanov 0404040404 Sydney Melbourne 45
 # createpackage IvanGeorgiev 0402020202 Sydney Perth 45
 # createroute 2025-02-16T14:30 Sydney Perth 
