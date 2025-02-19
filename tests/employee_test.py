a910efb527482e169c280b5d7eaf38b81a7d6930
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-02-19 12:47:54 +0000

    changes to employee tests.

--- tests/employee_test.py
+++ tests/employee_test.py
@@ -34,41 +34,41 @@ class TestEmployee(unittest.TestCase):
         with self.assertRaises(ValueError):
             employee.username = "LYU|BO"  # invalid symbol
 
-    def test_password_setter_valid(self):
+    def test_password_valid(self):
         employee = Employee(VALID_USERNAME, VALID_FIRST_NAME, VALID_LAST_NAME, VALID_PASSWORD, VALID_ROLE)
         employee.password = VALID_PASSWORD
         self.assertEqual(employee.password, VALID_PASSWORD)
 
-    def test_password_setter_invalid_length(self):
+    def test_password_invalid_length(self):
         employee = Employee(VALID_USERNAME, VALID_FIRST_NAME, VALID_LAST_NAME, VALID_PASSWORD, VALID_ROLE)
         with self.assertRaises(ValueError):
             employee.password = "1234"  # too short
         with self.assertRaises(ValueError):
             employee.password = "a" * 31  # too long
 
-    def test_password_setter_invalid_symbols(self):
+    def test_password_invalid_symbols(self):
         employee = Employee(VALID_USERNAME, VALID_FIRST_NAME, VALID_LAST_NAME, VALID_PASSWORD, VALID_ROLE)
         with self.assertRaises(ValueError):
             employee.password = "password@123"  # invalid symbol
 
-    def test_firstname_setter_valid(self):
+    def test_firstname_valid(self):
         employee = Employee(VALID_USERNAME, VALID_FIRST_NAME, VALID_LAST_NAME, VALID_PASSWORD, VALID_ROLE)
         employee.firstname = VALID_FIRST_NAME
         self.assertEqual(employee.firstname, VALID_FIRST_NAME)
 
-    def test_firstname_setter_invalid_length(self):
+    def test_firstname_invalid_length(self):
         employee = Employee(VALID_USERNAME, VALID_FIRST_NAME, VALID_LAST_NAME, VALID_PASSWORD, VALID_ROLE)
         with self.assertRaises(ValueError):
             employee.firstname = "L"  # too short
         with self.assertRaises(ValueError):
             employee.firstname = "P" * 21  # too long
 
-    def test_lastname_setter_valid(self):
+    def test_lastname_valid(self):
         employee = Employee(VALID_USERNAME, VALID_FIRST_NAME, VALID_LAST_NAME, VALID_PASSWORD, VALID_ROLE)
         employee.lastname = VALID_LAST_NAME
         self.assertEqual(employee.lastname, VALID_LAST_NAME)
 
-    def test_lastname_setter_invalid_length(self):
+    def test_lastname_invalid_length(self):
         employee = Employee(VALID_USERNAME, VALID_FIRST_NAME, VALID_LAST_NAME, VALID_PASSWORD, VALID_ROLE)
         with self.assertRaises(ValueError):
             employee.lastname = "S"  # too short
